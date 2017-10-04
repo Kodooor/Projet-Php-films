74e4a5fb06fd200d5459c6e0c6dc7a679bb02bbc
Juliette Dubernet <jdubernet@info12-28.iut45.univ-orleans.fr>
2017-10-04 18:01:00 +0200

    modification du css

--- accueuil.php
+++ accueuil.php
@@ -2,20 +2,55 @@
 <html>
 <head>
 <title>Search Film</title>
+<link rel="stylesheet" href="accueuil.css"/>
 </head>
 <header>
   <h1>Search Film </h1>
+  <?php
+  $theme=["Action"," Comédie","Drame", "Fiction","Horreur","Romance","Thriller"];
+  $criteres=[
+    array(
+      "type" => "text",
+      "name" => "recherchertitre",
+      "text" => "Rechercher avec le titre :",
+    )
+  ];
+
+  function question_text($q){
+    echo ("<p>" . $q["text"] . "<input type ='text' name='$q[name]'>" . "</p>");
+  }
+
+  $question_handlers = array(
+    "text" => "question_text",
+    "select" => "question_select"
+  );
+
+
+  //Boutons recherches
+    echo "<ol>";
+
+    foreach($theme as $t){
+    echo "<li><input type='submit' value=$t ></li>";
+  }
+
+  echo "</ol></form>";
+
+  //Rechercher un film
+
+  echo "<form method='POST' action='accueuil.php'><ol>";
+
+  echo "<div id='rechercher'>";
+
+  foreach($criteres as $c){
+    $question_handlers[$c["type"]]($c);}
+
+  echo "<input type='submit' value='Rechercher'></div>";
+
+   ?>
+
 </header>
 <body>
 <?php
-$theme=["Action"," Comédie","Drame", "Fiction","Horreur","Romance","Thriller"];
-$criteres=[
-  array(
-    "type" => "text",
-    "name" => "recherchertitre",
-    "text" => "Rechercher avec le titre :",
-  )
-];
 $tri=[
   array(
     "type" => "select",
@@ -41,10 +76,6 @@ $tri=[
   ])
 ];
 
-function question_text($q){
-  echo ("<p>" . $q["text"] . "</br><input type ='text' name='$q[name]'>" . "</p>");
-}
-
 function question_select($q){
   $html = "<p>" . $q["text"];
   $i = 0;
@@ -58,47 +89,24 @@ function question_select($q){
   echo $html;
 }
 
-$question_handlers = array(
-  "text" => "question_text",
-  "select" => "question_select"
-);
-
-
 // redirection des pages par les boutons
 
 
 // if ($_SERVER["REQUEST_METHOD"]=="GET"){
-  // Bouton recherche
 
-  echo "<form method='POST' action='accueuil.php'><ol>";
-  echo "Trouver film". "<br>";
 
-  foreach ($criteres as $c){
-    $question_handlers[$c["type"]]($c);
-  }
-
-  echo "<div><input type='submit' value='Rechercher'></div>";
-  echo "<ol>";
-
-  foreach($theme as $t){
-    echo "<li><input type='submit' value=$t ></li>";
-  }
-
-  echo "</ol></form>";
 
 
   // Ajouter
 
-  echo "Ajouter film". "<br>";
   echo "<form method='POST' action='ajouter_film.php'><ol>";
-  echo "<input type='submit' value='Ajouter'></form>". "<br>";
+  echo "<input type='submit' value='Ajouter un film'></form>". "<br>";
 
 
   // Supprimer film
 
-  echo "Supprimer film " . "<br>";
   echo "<form method='POST' action='supprimer_film.php'><ol>";
-  echo "<input type='submit' value='Supprimer'></form>". "<br>";
+  echo "<input type='submit' value='Supprimer un film'></form>". "<br>";
 
 
 
